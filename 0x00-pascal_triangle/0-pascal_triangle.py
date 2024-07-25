bb531cfdeb8da53663dea832770e0f2aaded60c7
root <smsmelshamey90@gmail.com>
2024-07-25 23:08:16 +0000

    classes

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Pascal triangle"""
+
+
 def pascal_triangle(n):
     """Create a function def pascal_triangle(n)
-    Returns: a list of lists of integers representing the Pascal’s triangle of n
+    Returns: a list of lists of integers repr Pascal’s triangle of n
     """
     if n <= 0:
         return []
