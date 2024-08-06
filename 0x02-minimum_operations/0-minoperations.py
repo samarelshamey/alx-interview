dda23c2bef235b2d135224a4e25fa3e8cf4754ff
root <smsmelshamey90@gmail.com>
2024-08-06 12:11:00 -0700

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,6 +2,16 @@
 """method to calculate minimum number of operaion needed"""
 
 def minOperations(n):
-    if n <= 0:
+    if n <= 1:
         return 0
-    
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
