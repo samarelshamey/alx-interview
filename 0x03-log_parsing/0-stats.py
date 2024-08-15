98c04ca13d1762f7755a50bd37ec66332e580341
root <smsmelshamey90@gmail.com>
2024-08-15 09:07:24 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import sys
 
 
@@ -24,17 +23,17 @@ total_file_size = 0
 code = 0
 counter = 0
 dict_sc = {"200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0}
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  
+        parsed_line = line.split()
         parsed_line = parsed_line[::-1]
 
         if len(parsed_line) > 2:
